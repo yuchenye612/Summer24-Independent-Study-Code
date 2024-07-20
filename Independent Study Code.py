2e151f6ebd80eb740a67a43ddb4a30ee7be1b022
Yuchen Ye yy959 <83521111+yuchenye612@users.noreply.github.com>
2024-07-19 23:54:25 -0400

    Update Independent Study Code.py

--- Independent Study Code.py
+++ Independent Study Code.py
@@ -1,27 +1,97 @@
 import pandas as pd
 import numpy as np
-import networkx as nx
 import scipy
 from scipy.optimize import minimize
 
-data = pd.read_csv('Traffic_Volume_Counts_20240707.csv').sample(n=50)
+data = pd.read_csv('Traffic_Volume_Counts_20240707.csv')#.sample(n=50)
+data['SegmentID'] = data['SegmentID'].astype(str).apply(lambda x: x.zfill(7))
 data = data.drop(['ID'], axis=1)
 data = data[data['From'].str.lower() != 'deadend']
 data = data[data['To'].str.lower() != 'deadend']
 data = data.replace({'(?i)avenue': 'AVE', '(?i)ave': 'AVE'}, regex=True)
 data = data.replace({'(?i)street': 'st', '(?i)st': 'st'}, regex=True)
 data = data.replace({'(?i)road': 'rd'}, regex=True)
+print('check1')
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 19 20:56:14 2024
+
+@author: yedot
+"""
+import geopandas as gpd
+from shapely.geometry import Point, MultiLineString
+
+from scipy.spatial.distance import pdist, squareform
+
+
+fp = "nyclion/lion.gdb"
+deta = gpd.read_file(fp, driver='FileGDB', layer='lion').sample(n=5000)
+
+deta = deta.replace({'(?i)avenue': 'AVE', '(?i)ave': 'AVE'}, regex=True)
+deta = deta.replace({'(?i)street': 'st', '(?i)st': 'st'}, regex=True)
+deta = deta.replace({'(?i)road': 'rd'}, regex=True)
+deta = deta[['SegmentID','geometry']]
+merged_df = pd.merge(data, deta, on='SegmentID',how='outer')
+
+df = merged_df.set_index('SegmentID').loc[deta['SegmentID']].reset_index()
+deta = df[['SegmentID','geometry']]
+print('check2')
+def multiline_to_first_point(multiline):
+    if isinstance(multiline, MultiLineString):
+        first_line = multiline.geoms[0]
+        first_point = first_line.coords[0]
+        return Point(first_point)
+    else:
+        return multiline
+
+pz = df['geometry'].apply(multiline_to_first_point)
+
+
+x_coords = gpd.GeoSeries(pz).x
+y_coords = gpd.GeoSeries(pz).y
+
+coordinates_matrix = gpd.pd.DataFrame({'x': x_coords, 'y': y_coords})
+
+dist_matrix = squareform(pdist(coordinates_matrix, 'euclidean'))
+print('check3')
+def reorder_points_nn(points, dist_matrix):
+    n = len(points)
+    visited = np.zeros(n, dtype=bool)
+    path = [0] 
+    visited[0] = True
+    
+    for _ in range(1, n):
+        last_point = path[-1]
+        nearest_neighbor = np.argmin(dist_matrix[last_point][~visited])
+        next_point = np.arange(n)[~visited][nearest_neighbor]
+        path.append(next_point)
+        visited[next_point] = True
+    
+    return path
+
+order = reorder_points_nn(coordinates_matrix, dist_matrix)
+print('check3')
+reordered_gdf = df.iloc[order]
+
+
+reordered_gdf = reordered_gdf.iloc[:, :-1]
+
+reordered_gdf = reordered_gdf.dropna(subset='12:00-1:00 AM')
+
+
+print('checkpoint')
 
 # Prepare nodes and edges
-nodes = pd.unique(data[['From', 'To']].values.ravel('K'))
+nodes = pd.unique(reordered_gdf[['From', 'To']].values.ravel('K'))
 nodes_list = list(nodes)
 node_index = {node: idx for idx, node in enumerate(nodes_list)}
 
-edges_raw = data.drop_duplicates(subset='SegmentID')
+edges_raw = reordered_gdf.drop_duplicates(subset='SegmentID')
 edges_raw = edges_raw[['SegmentID', 'Roadway Name', 'From', 'To', 'Direction', 'Date']]
-day_average_traffic = data.groupby('SegmentID').mean()
+day_average_traffic = reordered_gdf.groupby('SegmentID').mean(numeric_only=True).reset_index()
 unique_edge = pd.merge(edges_raw, day_average_traffic, on='SegmentID')
-unique_edge = unique_edge.groupby(['Roadway Name', 'From', 'To', 'SegmentID']).mean().reset_index()
+unique_edge = unique_edge.groupby(['Roadway Name', 'From', 'To', 'SegmentID'])._selected_obj
+#unique_edge = unique_edge.select_dtypes(include='number').mean()
 
 time_columns = [
     '12:00-1:00 AM', '1:00-2:00AM', '2:00-3:00AM', '3:00-4:00AM', '4:00-5:00AM', '5:00-6:00AM',
@@ -30,8 +100,8 @@ time_columns = [
     '6:00-7:00PM', '7:00-8:00PM', '8:00-9:00PM', '9:00-10:00PM', '10:00-11:00PM', '11:00-12:00AM'
 ]
 
-unique_edge['traffic_count'] = unique_edge[time_columns].apply(lambda row: row.tolist(), axis=1)
-traffic_count = pd.DataFrame(list(unique_edge['traffic_count']))
+traffic_count = unique_edge.iloc[:, 6:]
+#traffic_count = pd.DataFrame(list(unique_edge['traffic_count']))
 
 nodes = pd.unique(unique_edge[['From', 'To']].values.ravel('K'))
 nodes_list = list(nodes)
@@ -84,7 +154,7 @@ def loss_function(X, X_samp, mask, edge_laplacian, gamma):
     spatial_smoothness = np.sum([x.T @ edge_laplacian @ x for x in X.T])
     temporal_smoothness = np.sum([np.linalg.norm(X[:, t] - X[:, t-1])**2 for t in range(1, X.shape[1])])
 
-    return reconstruction_error + gamma * (temporal_smoothness)
+    return reconstruction_error + gamma * (spatial_smoothness + temporal_smoothness)
 
 result = minimize(loss_function, X_init, args=(X_samp, M, laplacian, gamma), method='L-BFGS-B')
 
